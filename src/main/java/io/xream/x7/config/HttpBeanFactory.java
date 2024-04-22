a081fa7e4be9969c15590070ff4b12f0c44bf207
x-ream <8966188@qq.com>
2024-04-22 20:09:42 +0800

    3.1.9

--- src/main/java/io/xream/x7/config/HttpBeanFactory.java
+++ src/main/java/io/xream/x7/config/HttpBeanFactory.java
@@ -1,9 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.xream.x7.config;
 
 
 import io.micrometer.common.util.StringUtils;
+import io.xream.x7.exception.ExchangeIOException;
+import io.xream.x7.exception.Remote3xxException;
+import io.xream.x7.exception.Remote4xxException;
+import io.xream.x7.exception.Remote5xxException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.support.RestTemplateAdapter;
 import org.springframework.web.context.request.RequestAttributes;
@@ -11,6 +34,9 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
@@ -54,12 +80,59 @@ public class HttpBeanFactory<T>  {
                 }
             }
 
-            return execution.execute(request, body);
+            ClientHttpResponse rep = execution.execute(request,body);
+            if (rep.getStatusCode().is2xxSuccessful() ) {
+                return rep;
+            }else if (rep.getStatusCode().is4xxClientError()){
+                String bodyStr = toBodyString(rep.getBody());
+                throw new Remote4xxException(rep.getStatusCode().value(),bodyStr);
+            }else if (rep.getStatusCode().is5xxServerError()){
+                String bodyStr = toBodyString(rep.getBody());
+                throw new Remote5xxException(rep.getStatusCode().value(),bodyStr);
+            }else if (rep.getStatusCode().is3xxRedirection()){
+                String bodyStr = toBodyString(rep.getBody());
+                throw new Remote3xxException(rep.getStatusCode().value(),bodyStr);
+            }
+            return rep;
         });
 
         RestTemplateAdapter adapter = RestTemplateAdapter.create(restTemplate);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
         return factory.createClient(interfaceType);
     }
+
+    public static String toBodyString(InputStream input) {
+        ByteArrayOutputStream output = null;
+        try {
+            output = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024 * 4];
+            int n = 0;
+            while (-1 != (n = input.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+            String str = new String(output.toByteArray(),"UTF-8");
+            return str;
+        }catch (Exception e) {
+            if (e instanceof RuntimeException)
+                throw (RuntimeException) e;
+            throw new ExchangeIOException(e.getMessage());
+        }finally {
+            if (output != null) {
+                try {
+                    output.close();
+                }catch (IOException ioe){
+                    throw new ExchangeIOException(ioe.getMessage());
+                }
+            }
+            if (input !=null) {
+                try {
+                    input.close();
+                }catch (IOException ioe){
+                    throw new ExchangeIOException(ioe.getMessage());
+                }
+            }
+        }
+    }
+
 
 }
